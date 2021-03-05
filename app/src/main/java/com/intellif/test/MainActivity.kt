e149fbc76c24cb035221824ab6a63359efb26090
zyp <2565495>
2021-03-05 19:12:24 +0800

    上传笔记和代码

--- app/src/main/java/com/intellif/test/MainActivity.kt
+++ app/src/main/java/com/intellif/test/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.intellif.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linkedList = LinkedList<Int>()
+        linkedList.last
+        linkedList.first
     }
+
+    val client = OkHttpClient.Builder()
+        //            .addInterceptor()
+        .build()
+
+    @Throws(IOException::class)
+    fun run(url: String): String? {
+        val client = OkHttpClient.Builder()
+
+            .build()
+        val request: Request = Request.Builder()
+            .url(url)
+            .build()
+        //异步
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+            }
+
+        })
+        //同步
+        client.newCall(request).execute().use { response -> return response.body?.string() }
+
+    }
+
+
 }
